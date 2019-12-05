0806f12494ab7e7a16237e634231f8166ea2e8d7
Pavel <ponomarenko.pavel.web@gmail.com>
2019-12-05 20:14:27 +0200

    set variables, style for header/footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="footer js-footer">
+<footer class="footer">
     <?php if (is_active_sidebar('footer-widget-area')) : ?>
         <div class="pre-footer">
             <div class="container">
@@ -8,7 +8,62 @@
             </div>
         </div><!-- .pre-footer end-->
     <?php endif; ?>
-
+    <div class="footer__bg">
+        <div class="container-fluid padding-none">
+            <div class="footer__wrapper">
+                <div class="footer__column">
+                    <div class="footer__logo">
+                        <?php get_default_logo_link([
+                            'name'    => 'logo-footer.svg',
+                            'options' => [
+                                'class'  => 'logo-img',
+                                'width'  => 200,
+                                'height' => 50,
+                            ],
+                        ])
+                        ?>
+                    </div>
+                    <div class="footer__list">
+                        <div class="footer__field">
+                            <div class="footer__icon">
+                                <i class="fas fa-map-marker-alt"></i>
+                            </div>
+                            <div class="footer__description">
+                                <?php pll_e('Address') ?>
+                            </div>
+                        </div>
+                        <div class="footer__field">
+                            <div class="footer__icon">
+                                <i class="far fa-clock"></i>
+                            </div>
+                            <div class="footer__description">
+                                <?php pll_e('Work Schedule') ?>
+                            </div>
+                        </div>
+                        <div class="footer__field">
+                            <div class="footer__icon">
+                                <i class="fas fa-mobile-alt"></i>
+                            </div>
+                            <div class="footer__description">
+                                <?php echo do_shortcode('[bw-phone]'); ?>
+                            </div>
+                        </div>
+                        <div class="footer__field">
+                            <div class="footer__sub-title">
+                                <?php pll_e('123') ?>
+                            </div>
+                            <div class="footer__description">
+                                <?php echo do_shortcode('[bw-social]'); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer__map-wrapper">
+                    <iframe width="994" height="754" id="gmap_canvas" src="https://maps.google.com/maps?q=%D0%B8%D1%80%D0%BF%D0%B5%D0%BD%D1%8C&t=&z=17&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <div class="copyright">
             <div class="date">&copy; <?php echo date('Y'); ?>. Все права защищены</div>
--- header.php
+++ header.php
@@ -36,41 +36,96 @@
 <div class="wrapper js-container"><!--Do not delete!-->
 
     <header class="page-header fixed-to-top">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2">
-                    <div class="logo">
-	                    <?php get_default_logo_link([
-                            'name'    => 'logo.svg',
-                            'options' => [
-                                'class'  => 'logo-img',
-                                'width'  => 100,
-                                'height' => 50,
+        <div class="page-header__top-section">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                        <div class="logo">
+                            <?php get_default_logo_link([
+                                'name'    => 'logo.svg',
+                                'options' => [
+                                    'class'  => 'logo-img',
+                                    'width'  => 200,
+                                    'height' => 50,
                                 ],
                             ])
-                        ?>
+                            ?>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
+                        <div class="page-header__wrapper">
+                            <div class="page-header__item">
+                                <div class="page-header__icon">
+                                    <i class="fas fa-map-marker-alt"></i>
+                                </div>
+                                <?php pll_e('Address') ?>
+                            </div>
+                            <div class="page-header__item">
+                                <div class="page-header__icon">
+                                    <i class="far fa-clock"></i>
+                                </div>
+                                <?php pll_e('Work Schedule') ?>
+                            </div>
+                            <div class="page-header__item">
+                                <?php echo do_shortcode('[bw-social]'); ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+                        <div class="page-header__item-wrapper">
+                            <div class="page-header__item">
+                                <div class="page-header__icon">
+                                    <i class="fas fa-mobile-alt"></i>
+                                </div>
+                                <?php if (has_phones()) { ?>
+                                    <ul class="phone-dropdown">
+                                        <li class="phone-dropdown__item">
+                                            <?php foreach(get_phones() as $key => $phone) { ?>
+                                            <?php reset(get_phones()); ?>
+                                            <?php if ($key === key(get_phones())) { ?>
+                                            <a href="tel:<?php echo esc_attr(get_phone_number($phone)); ?>" class="phone-dropdown__link phone-dropdown--main">
+                                                <?php echo esc_html($phone); ?>
+                                            </a>
+                                            <button type="button" class="phone-dropdown__button js-dropdown"></button>
+                                            <ul class="phone-dropdown__list js-phone-list">
+                                                <?php  } else { ?>
+                                                    <li class="phone-dropdown__item">
+                                                        <a href="tel:<?php echo esc_attr(get_phone_number($phone)); ?>" class="phone-dropdown__link">
+                                                            <?php echo esc_html($phone); ?>
+                                                        </a>
+                                                    </li>
+                                                <?php } ?>
+                                                <?php } ?>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                <?php } ?>
+                            </div>
+                            <button type="button" class="button-main button-main--style <?php the_lang_class('js-call-back'); ?>">
+                                <?php _e('Call back', 'brainworks'); ?>
+                            </button>
+                        </div>
                     </div>
                 </div>
-                <div class="col-12 col-sm-12 col-md-7 col-lg-7 col-xl-7">
+            </div>
+        </div>
+        <div class="page-header__bottom-section">
+            <div class="container">
+                <div class="row align-items-center">
                     <?php if (has_nav_menu('main-nav')) { ?>
                         <nav class="nav js-menu">
                             <button type="button" tabindex="0" class="menu-item-close menu-close js-menu-close"></button>
                             <?php wp_nav_menu(array(
-                            'theme_location' => 'main-nav',
-                            'container' => false,
-                            'menu_class' => 'menu-container',
-                            'menu_id' => '',
-                            'fallback_cb' => 'wp_page_menu',
-                            'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                            'depth' => 3
+                                'theme_location' => 'main-nav',
+                                'container' => false,
+                                'menu_class' => 'menu-container',
+                                'menu_id' => '',
+                                'fallback_cb' => 'wp_page_menu',
+                                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                                'depth' => 3
                             )); ?>
                         </nav>
                     <?php } ?>
-                </div>
-                <div class="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
-                    <button type="button" class="btn btn-secondary btn-block <?php the_lang_class('js-call-back'); ?>">
-                        <?php _e('Call back', 'brainworks'); ?>
-                    </button>
                 </div>
             </div>
         </div>
